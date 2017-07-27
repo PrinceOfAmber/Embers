be750cb714824928c5c30da95c9a2b6718e92c8f
sam <samson.bassett@gmail.com>
2017-07-26 23:31:08 -0700

    more unused packets

--- src/main/java/teamroots/embers/network/PacketHandler.java
+++ src/main/java/teamroots/embers/network/PacketHandler.java
@@ -13,14 +13,14 @@ public class PacketHandler {
 
     public static void registerMessages() {
         //INSTANCE.registerMessage(MessageTEUpdate.MessageHolder.class, MessageTEUpdate.class, id++, Side.CLIENT);
-        INSTANCE.registerMessage(MessageCannonBeamFX.MessageHolder.class, MessageCannonBeamFX.class, id++, Side.CLIENT);
+      //  INSTANCE.registerMessage(MessageCannonBeamFX.MessageHolder.class, MessageCannonBeamFX.class, id++, Side.CLIENT);
         INSTANCE.registerMessage(MessageEmberBurstFX.MessageHolder.class, MessageEmberBurstFX.class, id++, Side.CLIENT);
         //INSTANCE.registerMessage(MessageItemUpdate.MessageHolder.class, MessageItemUpdate.class, id++, Side.CLIENT);
         INSTANCE.registerMessage(MessageEmberSparkleFX.MessageHolder.class, MessageEmberSparkleFX.class, id++, Side.CLIENT);
         INSTANCE.registerMessage(MessageEmberSphereFX.MessageHolder.class, MessageEmberSphereFX.class, id++, Side.CLIENT);
-        INSTANCE.registerMessage(MessageBeamCannonFX.MessageHolder.class, MessageBeamCannonFX.class, id++, Side.CLIENT);
+      //  INSTANCE.registerMessage(MessageBeamCannonFX.MessageHolder.class, MessageBeamCannonFX.class, id++, Side.CLIENT);
        // INSTANCE.registerMessage(MessageStamperFX.MessageHolder.class, MessageStamperFX.class, id++, Side.CLIENT);
-        INSTANCE.registerMessage(MessageAnvilSparksFX.MessageHolder.class, MessageAnvilSparksFX.class, id++, Side.CLIENT);
+        //INSTANCE.registerMessage(MessageAnvilSparksFX.MessageHolder.class, MessageAnvilSparksFX.class, id++, Side.CLIENT);
         INSTANCE.registerMessage(MessageTyrfingBurstFX.MessageHolder.class, MessageTyrfingBurstFX.class, id++, Side.CLIENT);
       //  INSTANCE.registerMessage(MessageEmberActivationFX.MessageHolder.class, MessageEmberActivationFX.class, id++, Side.CLIENT);
         INSTANCE.registerMessage(MessageEmberGenOffset.MessageHolder.class, MessageEmberGenOffset.class, id++, Side.CLIENT);
