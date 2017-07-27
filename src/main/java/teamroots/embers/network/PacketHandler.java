761521070cb93eab9319679b5a28868ad49fda58
sam <samson.bassett@gmail.com>
2017-07-26 23:35:06 -0700

    more unused packets

--- src/main/java/teamroots/embers/network/PacketHandler.java
+++ src/main/java/teamroots/embers/network/PacketHandler.java
@@ -14,7 +14,7 @@ public class PacketHandler {
     public static void registerMessages() {
         //INSTANCE.registerMessage(MessageTEUpdate.MessageHolder.class, MessageTEUpdate.class, id++, Side.CLIENT);
       //  INSTANCE.registerMessage(MessageCannonBeamFX.MessageHolder.class, MessageCannonBeamFX.class, id++, Side.CLIENT);
-        INSTANCE.registerMessage(MessageEmberBurstFX.MessageHolder.class, MessageEmberBurstFX.class, id++, Side.CLIENT);
+       // INSTANCE.registerMessage(MessageEmberBurstFX.MessageHolder.class, MessageEmberBurstFX.class, id++, Side.CLIENT);
         //INSTANCE.registerMessage(MessageItemUpdate.MessageHolder.class, MessageItemUpdate.class, id++, Side.CLIENT);
         INSTANCE.registerMessage(MessageEmberSparkleFX.MessageHolder.class, MessageEmberSparkleFX.class, id++, Side.CLIENT);
         INSTANCE.registerMessage(MessageEmberSphereFX.MessageHolder.class, MessageEmberSphereFX.class, id++, Side.CLIENT);
@@ -23,7 +23,7 @@ public class PacketHandler {
         //INSTANCE.registerMessage(MessageAnvilSparksFX.MessageHolder.class, MessageAnvilSparksFX.class, id++, Side.CLIENT);
         INSTANCE.registerMessage(MessageTyrfingBurstFX.MessageHolder.class, MessageTyrfingBurstFX.class, id++, Side.CLIENT);
       //  INSTANCE.registerMessage(MessageEmberActivationFX.MessageHolder.class, MessageEmberActivationFX.class, id++, Side.CLIENT);
-        INSTANCE.registerMessage(MessageEmberGenOffset.MessageHolder.class, MessageEmberGenOffset.class, id++, Side.CLIENT);
+      //  INSTANCE.registerMessage(MessageEmberGenOffset.MessageHolder.class, MessageEmberGenOffset.class, id++, Side.CLIENT);
         INSTANCE.registerMessage(MessageSuperheatFX.MessageHolder.class, MessageSuperheatFX.class, id++, Side.CLIENT);
       //  INSTANCE.registerMessage(MessagePlayerJetFX.MessageHolder.class, MessagePlayerJetFX.class, id++, Side.CLIENT);
         INSTANCE.registerMessage(MessageFlameShieldFX.MessageHolder.class, MessageFlameShieldFX.class, id++, Side.CLIENT);
